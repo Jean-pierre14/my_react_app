66f78f2bc89b26c714c95106ddf37d76218a328b
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-01-14 22:11:20 +0530

    Racine app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h3>App code</h3>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/assets/index.js
@@ -0,0 +1,5 @@
+const imgs = [
+  { id: "234e", name: "Grace", age: 26 },
+  { id: "2e34", name: "Daniel", age: 13 },
+  { id: "23e4", name: "Adolf", age: 18 },
+];
